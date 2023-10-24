95826d27968557d6aaa60ada0adbb53f856471f2
RolandNeuber <rolandmartinneuber@gmail.com>
2023-10-24 20:15:29 +0200

    Projektdateien hinzufügen.

---
+++ BitReverse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourierTransform
+{
+	public static class BitReverse
+	{
+		public static T[] ReverseBits<T>(IList<T> list)
+		{
+			if (!BitOperations.IsPow2(list.Count))
+				throw new ArgumentException("list length must be power of two.");
+			T[] reverse = new T[list.Count];
+            for (int i = 0; i < list.Count; i++)
+			{
+				uint reversedIndex = ReverseBits((uint)i, (uint)BitOperations.Log2((uint)list.Count));
+                reverse[reversedIndex] = list[i];
+			}
+            return reverse;
+		}
+		public static uint ReverseBits(uint number, uint maxBits)
+		{
+			if (maxBits > 32)
+				throw new ArgumentOutOfRangeException(nameof(maxBits));
+			uint reverse = 0;
+			for (int i = 0; i < maxBits; i++)
+			{
+				reverse <<= 1;
+				reverse |= number & 1;
+				number >>= 1;
+			}
+			return reverse;
+		}
+	}
+}
---
+++ FourierTransform.cs
@@ -0,0 +1,42 @@
+﻿using FourierTransform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourierTransform
+{
+	public static class FourierTransforms
+	{
+		/// <summary>
+		/// Computes the DFT of a given Time-Domain-Signal.
+		/// Time-Domain-Signal can be of any size, but is very slow for non-powers of 2.
+		/// Try to avoid Time-Domain-Signals with non-power of two sizes.
+		/// </summary>
+		/// <param name="timeDomainSignal">The Time-Domain-Signal.</param>
+		/// <returns>Returns the corresponding Frequency-Domain-Signal.</returns>
+		public static Complex[] DFT(IList<Complex> timeDomainSignal)
+		{
+			if (timeDomainSignal.Count == 0)
+				return timeDomainSignal.ToArray();
+			if (BitOperations.IsPow2(timeDomainSignal.Count))
+				return FourierTransformOperations.ParallelIterativeDFT(timeDomainSignal);
+			return FourierTransformOperations.DFT(timeDomainSignal);
+			//Complex[] extendedTimeDomainSignal = new Complex[BitOperations.RoundUpToPowerOf2((uint)timeDomainSignal.Count)];
+			//Array.Copy(timeDomainSignal.ToArray(), extendedTimeDomainSignal, timeDomainSignal.Count);
+
+			//for (int i = timeDomainSignal.Count; i < extendedTimeDomainSignal.Length; i++)
+			//{
+			//	extendedTimeDomainSignal[i] = Complex.Zero;
+			//}
+			
+			//Complex[] frequencyDomainSignal = FourierTransformOperations.ParallelIterativeDFT(extendedTimeDomainSignal);
+			//Complex[] truncatedFrequencyDomainSignal = new Complex[timeDomainSignal.Count];
+			//Array.Copy(frequencyDomainSignal, truncatedFrequencyDomainSignal, timeDomainSignal.Count);
+			
+			//return truncatedFrequencyDomainSignal;
+		}
+	}
+}
